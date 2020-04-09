c493c41eeae487abf893acc15d28547b80a4669c
Kshem Sharma <kshemsharma@Kshems-MacBook-Pro.local>
2020-04-09 10:20:01 +0530

    add results route

--- movie_search/app.js
+++ movie_search/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const app = express();
+var app = express();
 const rp = require('request-promise');
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 app.get('/results', (req, res) => {
-	res.render('Hello');
+	rp('http://www.omdbapi.com/?apikey=thewdb&i=tt2015381')
+		.then((body) => {
+			const parsedData = JSON.parse(body);
+			console.log(parsedData);
+			res.render('result', { data: parsedData });
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
-// rp("http://www.omdbapi.com/?apikey=thewdb&i=tt2015381")
-// .then((body)=>{
-//     console.log(body);
-// })
-// .catch((err)=>{
-//     console.log(err);
-// })
 app.listen(PORT, () => {
 	console.log(`Server has started at ${PORT}`);
 });
