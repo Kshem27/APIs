904feb3e8af936b4b83abdd4bc770f7ec992c72e
Kshem Sharma <kshemsharma@Kshems-MacBook-Pro.local>
2020-04-10 06:47:38 +0530

    add search route

--- movie_search/app.js
+++ movie_search/app.js
@@ -4,15 +4,19 @@ const rp = require('request-promise');
 const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.get('/results', (req, res) => {
-	rp('http://www.omdbapi.com/?apikey=thewdb&i=tt2015381')
+	var movieName = req.query.s;
+	rp('http://www.omdbapi.com/?apikey=thewdb&s=' + movieName)
 		.then((body) => {
 			const parsedData = JSON.parse(body);
 			console.log(parsedData);
-			res.render('result', { data: parsedData });
+			res.render('result', { allData: parsedData });
 		})
 		.catch((err) => {
 			console.log(err);
 		});
+});
+app.get('/', (req, res) => {
+	res.render('search');
 });
 app.listen(PORT, () => {
 	console.log(`Server has started at ${PORT}`);
