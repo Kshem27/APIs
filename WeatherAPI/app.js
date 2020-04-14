a824cbf57ac36ad7fdd091b700a8ddfc2d40e3c2
Kshem Sharma <kshemsharma@Kshems-MacBook-Pro.local>
2020-04-14 22:49:33 +0530

    update results route

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const rp = require('request-promise');
 var app = express();
 var PORT = 3000 || process.env.PORT;
-rp('https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=83357010e805049f8a9d009658c7c1f0')
-	.then((body) => {
-		var parsedData = JSON.parse(body);
-		//eval(require('locus'));
-		console.log(parsedData);
-	})
-	.catch((err) => {
-		console.log('Err', err);
-	});
+app.set('view engine', 'ejs');
+app.get('/results', (req, res) => {
+	rp('https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=83357010e805049f8a9d009658c7c1f0')
+		.then((body) => {
+			var parsedData = JSON.parse(body);
+			//eval(require('locus'));
+			console.log(parsedData);
+			res.render('result', { data: parsedData });
+		})
+		.catch((err) => {
+			console.log('Err', err);
+		});
+});
+
 app.listen(PORT, () => {
 	console.log(`Server has started at PORT::${PORT}`);
 });
