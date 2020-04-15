37f43b46c99c3d43dca1d2c5deab9644bcff68d9
Kshem Sharma <kshemsharma@Kshems-MacBook-Pro.local>
2020-04-15 07:59:00 +0530

    error route

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -4,16 +4,35 @@ var app = express();
 var PORT = 3000 || process.env.PORT;
 app.set('view engine', 'ejs');
 app.get('/results', (req, res) => {
-	rp('https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=83357010e805049f8a9d009658c7c1f0')
-		.then((body) => {
-			var parsedData = JSON.parse(body);
-			//eval(require('locus'));
-			console.log(parsedData);
-			res.render('result', { data: parsedData });
-		})
-		.catch((err) => {
-			console.log('Err', err);
-		});
+	var cityName = req.query.q;
+	if (cityName) {
+		rp(
+			'https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=83357010e805049f8a9d009658c7c1f0&q=' +
+				cityName
+		)
+			.then((body) => {
+				var parsedData = JSON.parse(body);
+				//eval(require('locus'));
+				console.log(parsedData);
+				res.render('result', { data: parsedData });
+			})
+			.catch((err) => {
+				console.log('Err', err);
+			});
+	} else
+		rp(
+			'https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=83357010e805049f8a9d009658c7c1f0&q=delhi'
+		)
+			.then((body) => {
+				var parsedData = JSON.parse(body);
+				//eval(require('locus'));
+				console.log(parsedData);
+				if (parsedData.cod == 200) res.render('result', { data: parsedData });
+				else res.render('error');
+			})
+			.catch((err) => {
+				console.log('Err', err);
+			});
 });
 
 app.listen(PORT, () => {
